5591ce779b20c890bd4905e41141ab3eeac770fb
Diego Rivera <93798677+Diegorivera1110@users.noreply.github.com>
2023-01-07 19:44:36 -0800

    added solve function

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,15 @@
+// function for dsiplay
 function display(val) {
     document.getElementById('result').value += val
     return val
+}
+
+// Solving function
+function solve() {
+    let x = document.getElementById('result').value
+    let y = eval(x);
+
+    document.getElementById('result').value = y
+
+    return y
 }
