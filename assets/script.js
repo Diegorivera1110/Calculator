16da33689dc66ddde8857cf4ced6ebe8bb2f3ab6
Diego Rivera <93798677+Diegorivera1110@users.noreply.github.com>
2023-01-10 17:08:40 -0800

    added last funciton to clear result box

--- assets/script.js
+++ assets/script.js
@@ -1,15 +1,19 @@
 // function for dsiplay
 function display(val) {
-    document.getElementById('result').value += val
-    return val
+  document.getElementById("result").value += val;
+  return val;
 }
 
 // Solving function
 function solve() {
-    let x = document.getElementById('result').value
-    let y = eval(x);
+  let x = document.getElementById("result").value;
+  let y = eval(x);
 
-    document.getElementById('result').value = y
+  document.getElementById("result").value = y;
 
-    return y
+  return y;
+}
+
+function clearScreen() {
+  document.getElementById("result").value = "";
 }
